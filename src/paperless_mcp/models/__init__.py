af4816d31af83a696c9361e800d07391f99b0a49
Peter van Liesdonk <peter@liesdonk.nl>
2026-04-23 19:16:44 +0200

    feat(models): add Document, DocumentPatch, metadata, notes, history, suggestions

--- src/paperless_mcp/models/__init__.py
+++ src/paperless_mcp/models/__init__.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 from paperless_mcp.models.common import (
     BulkEditOperation, BulkEditResult, DownloadLink, ListParams, Paginated, UploadTaskAcknowledgement,
 )
+from paperless_mcp.models.document import (
+    CustomFieldInstance, Document, DocumentHistoryEntry, DocumentMetadata, DocumentNote, DocumentPatch, DocumentSuggestions,
+)
 __all__ = [
     "BulkEditOperation", "BulkEditResult", "DownloadLink", "ListParams", "Paginated", "UploadTaskAcknowledgement",
+    "CustomFieldInstance", "Document", "DocumentHistoryEntry", "DocumentMetadata", "DocumentNote", "DocumentPatch", "DocumentSuggestions",
 ]
